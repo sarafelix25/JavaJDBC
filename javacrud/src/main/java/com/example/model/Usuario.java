b44fff309b8c233252f4cc03514228aaf0e150ab
Sara Felix <70541669+sarafelix25@users.noreply.github.com>
2024-05-05 17:35:11 +0000

    criação da model

---
+++ javacrud/src/main/java/com/example/model/Usuario.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Usuario {
+    private int id;
+    private String nome;
+    private String email;
+
+    public Usuario(int id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+}
